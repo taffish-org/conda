ed84d0e23f7a678de6319af5d0305d4cb544c7fa
Kaiyuan Han <hermit_han@163.com>
2025-03-29 17:24:36 +0800

    Initial commit

---
+++ conda.info.lisp
@@ -0,0 +1,8 @@
+(:name           "conda")
+(:repo           "taffish-org/conda")
+(:author         "KaiyuanHan")
+(:version        "3.12")
+(:intro          "The world’s most trusted open ecosystem for sourcing, building, and deploying data science and AI initiatives")
+(:dependencies)
+(:functions      "conda"
+                 "[python 3.12]")
